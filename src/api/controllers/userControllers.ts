783b4824978daa20e8a1c9174cc2b69a8606fb53
sohidul-kadritech <sohidul.islam@kadritech.se>
2024-05-02 02:32:50 +0600

    filtering practice

--- src/api/controllers/userControllers.ts
+++ src/api/controllers/userControllers.ts
@@ -1,29 +1,40 @@
-import { Body, Get, JsonController, Param, Post, QueryParam, QueryParams } from "routing-controllers";
+import {
+  Body,
+  Get,
+  JsonController,
+  Param,
+  Post,
+  QueryParam,
+  QueryParams,
+} from "routing-controllers";
 import { UserService } from "../services/userService";
 import Container from "typedi";
 
-@JsonController('/user')
+@JsonController("/user")
 export class ProfileController {
+  private userService = Container.get(UserService);
 
-    private userService = Container.get(UserService);
-
-  @Get('/')
+  @Get("/")
   async getUser(@QueryParams() params: any) {
     return await this.userService.getUsers(params);
   }
-
-  @Post('/')
-  async createUser(@Body({required:false}) request: any) {
-    return await this.userService.createUser(request)
+  @Get("/filter")
+  async getUsers(@QueryParams() params: any) {
+    return await this.userService.getUsersByFilter(params);
   }
 
-  @Post('/update')
-  async updateUsers(@Body({required:false}) request: any) {
-    return await this.userService.updateUsers(request)
+  @Post("/")
+  async createUser(@Body({ required: false }) request: any) {
+    return await this.userService.createUser(request);
   }
 
-  @Post('/delete')
-  async deleteUser(@Body({required:false}) request: any) {
-    return await this.userService.deleteUser(request)
+  @Post("/update")
+  async updateUsers(@Body({ required: false }) request: any) {
+    return await this.userService.updateUsers(request);
+  }
+
+  @Post("/delete")
+  async deleteUser(@Body({ required: false }) request: any) {
+    return await this.userService.deleteUser(request);
   }
 }
--- src/api/services/userService.ts
+++ src/api/services/userService.ts
@@ -1,119 +1,164 @@
 import { Service } from "typedi";
-import { Repository } from "typeorm";
+import { ILike, Repository } from "typeorm";
 // import { InjectRepository } from "typeorm-typedi-extensions";
 import User from "../entities/User";
 import { InjectRepository } from "typeorm-typedi-extensions";
 
-
 @Service()
 export class UserService {
-  
-  constructor (
+  constructor(
     @InjectRepository(User)
-    private userRepo : Repository<User>
-  ){
+    private userRepo: Repository<User>
+  ) {}
 
-  }
+  async createUser(request: any) {
+    const user = await this.userRepo.create(request);
+    const newUser = this.userRepo.save(request);
 
-    async createUser (request:any){
-         
-    const user = await this.userRepo.create(request)   
-    const newUser = this.userRepo.save(request)     
-        
-      return {
-        succss:"success",
-        data: "create user",
-        entry: newUser,
-      }
-    }  
-
-    async getUsers (params:any){
-
-         // Extract any criteria from the request object to filter the user records
-         const { name,id } = params;
-        
-         // Define selection conditions based on the extracted criteria
-         const conditions: any = {};
-         if (name) {
-             conditions.name = name;
-         }
-
-         if(id){
-          conditions.id=id;
-         }
-
-        
-      // const user = await this.userRepo.findOneBy(conditions)  
-      
-      const users = await this.userRepo.findBy(conditions)
-
-      
-      if(users?.length){
-        return {
-          success: "success",
-          message: "user found",
-          data: users,
-      };
-      }
-      
     return {
-      succss:"failed",
-      message: "User Not found",
-    }
+      succss: "success",
+      data: "create user",
+      entry: newUser,
+    };
   }
-  
-  
-  async updateUsers (data:any){
-    const {id}  = data;
 
-    
+  async getUsers(params: any) {
+    // Extract any criteria from the request object to filter the user records
+    const { name, id } = params;
+
+    // Define selection conditions based on the extracted criteria
+    const conditions: any = {};
+    if (name) {
+      conditions.name = name;
+    }
+
+    if (id) {
+      conditions.id = id;
+    }
+
+    // const user = await this.userRepo.findOneBy(conditions)
+
+    const users = await this.userRepo.findBy(conditions);
+
+    if (users?.length) {
+      return {
+        success: "success",
+        message: "user found",
+        data: users,
+      };
+    }
+
+    return {
+      succss: "failed",
+      message: "User Not found",
+    };
+  }
+
+  async getUsersByFilter(params: any) {
+    const { name, id, page = 1, pageSize = 10, searchKey } = params;
+
+    // Define selection conditions based on the extracted criteria
+    const conditions: any = {};
+    if (name) {
+      conditions.name = name;
+    }
+
+    if (id) {
+      conditions.id = id;
+    }
+
+    if (searchKey) {
+      conditions.or = [
+        { name: ILike(`%${searchKey}%`) },
+        { email: ILike(`%${searchKey}%`) },
+      ];
+    }
+
+    // if (searchKey) {
+    //   conditions.name = ILike(`%${searchKey}%`);
+    // }
+
+    // if (searchKey) {
+    //   conditions.email = ILike(`%${searchKey}%`);
+    // }
+
+    // Calculate skip value for pagination
+    const skip = (page - 1) * pageSize;
+
+    // Find users based on conditions with pagination
+    const [users, totalUsers] = await this.userRepo.findAndCount({
+      where: [
+        { name: ILike(`%${searchKey}%`) },
+        { email: ILike(`%${searchKey}%`) },
+      ],
+      // where: conditions,
+      skip,
+      take: pageSize,
+    });
+
+    const totalPages = Math.ceil(totalUsers / pageSize);
+
+    if (users?.length) {
+      return {
+        success: "success",
+        message: "user found",
+        data: users,
+        metaData: {
+          page,
+          pageSize,
+          totalPages,
+          totalData: totalUsers,
+        },
+      };
+    }
+
+    return {
+      succss: "failed",
+      message: "User Not found",
+      data: [],
+    };
+  }
+
+  async updateUsers(data: any) {
+    const { id } = data;
+
     const user = await this.userRepo.findOneById(id);
 
-    
-
-    if(user){
-      
-      const update = await this.userRepo.update({id},data)
+    if (user) {
+      const update = await this.userRepo.update({ id }, data);
 
       return {
-        success:"success",
+        success: "success",
         message: "Successfully Updated",
         data: data,
-}
-      
+      };
     }
 
     return {
-      succss:"failed",
+      succss: "failed",
       message: "User Not found",
-    }
-}
+    };
+  }
 
-  
-  async deleteUser (data:any){
-    const {id}  = data;
+  async deleteUser(data: any) {
+    const { id } = data;
 
-    
     const user = await this.userRepo.findOneById(id);
 
-    
-
-    if(user){
-      
-      const deletedData = await this.userRepo.delete(id)
+    if (user) {
+      const deletedData = await this.userRepo.delete(id);
 
       return {
-        success:"success",
+        success: "success",
         message: "Successfully Deleted",
         data: user,
-        deletedData
-        }
-      
+        deletedData,
+      };
     }
 
     return {
-      succss:"failed",
+      succss: "failed",
       message: "User Not found",
-    }
-}
+    };
+  }
 }
